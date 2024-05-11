d4de2691a1fea62aaf709732d0cd6c92eadb2bdc
EmileNapon <emilenapon0@gmail.com>
2024-05-11 19:04:53 +0000

    projet de culture generale

--- chapitre2/src/app/Message/messageService/messageService.ts
+++ chapitre2/src/app/Message/messageService/messageService.ts
@@ -1,4 +1,20 @@
 export class MessageService{
     public date = new Date()
-    public contenu =''
+    public ContenuNapon =[{}]
+    public ContenuNacro =[{}]
+    public ContenuKabore =[{}]
+    public ContenuSome =[{}]
+
+    addNapon(contenu:string){
+         this.ContenuNapon.push(contenu)
+    }
+    addNacro(contenu:string){
+        this.ContenuNacro.push(contenu)
+   }
+   addKabore(contenu:string){
+    this.ContenuKabore.push(contenu)
+   }
+   addSome(contenu:string){
+    this.ContenuSome.push(contenu)
+     }
 }
--- chapitre2/src/app/user/user.component.ts
+++ chapitre2/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import{Component, OnInit} from '@angular/core'
 import { UserService } from './userService/userService';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MessageService } from '../Message/messageService/messageService';
 
 @Component({
     selector:'user-root',
@@ -11,22 +12,126 @@ export class UserComponent implements OnInit{
 
     public listUser:any
 
-    constructor(private userservice:UserService, private fb:FormBuilder){}
+    constructor(private userservice:UserService,private ms:MessageService, private fb:FormBuilder){}
     ngOnInit(): void {
-     this.listUser=this.userservice.listUser  
-     this.initForm() 
+     this.listUser=this.userservice.listUser
+     this.initFormNapon() 
+     this.initFormNacro()
+     this.initFormKabore()
+     this.initFormSome()
+     this.ms.ContenuNapon
     }
-    public myF!:FormGroup
+    public myFNapon!:FormGroup
+    public myFNacro!:FormGroup
+    public myFKabore!:FormGroup
+    public myFSome!:FormGroup
 
-    initForm(){
-        this.myF=this.fb.group({
+    initFormNapon(){
+        this.myFNapon=this.fb.group({
             contenu:'',
             ch:''
         })
     }
-    onSubmit(){
-        this.myF.value
-        console.log(this.myF.value)
+    initFormNacro(){
+        this.myFNacro=this.fb.group({
+            contenu:'',
+            ch:''
+        })
+    }
+    initFormKabore(){
+        this.myFKabore=this.fb.group({
+            contenu:'',
+            ch:''
+        })
+    }
+ 
+    initFormSome(){
+        this.myFSome=this.fb.group({
+            contenu:'',
+            ch:''
+        })
+    }
+
+
+
+    onSubmitNapon(){
+        if(this.myFNapon.value['ch']=='Napon'){
+           this.ms.addNapon(this.myFNapon.value['contenu'])
+           console.log(this.ms.ContenuNapon)
+        }
+
+        if(this.myFNapon.value['ch']=='Nacro'){
+            this.ms.addNacro(this.myFNapon.value['contenu'])
+            console.log(this.ms.ContenuNacro)
+         }
+         if(this.myFNapon.value['ch']=='Kabore'){
+            this.ms.addKabore(this.myFNapon.value['contenu'])
+            console.log(this.ms.ContenuKabore)
+         }
+         if(this.myFNapon.value['ch']=='Some'){
+            this.ms.addSome(this.myFNapon.value['contenu'])
+            console.log(this.ms.ContenuSome)
+         }
+    }
+
+    onSubmitNacro(){
+        if(this.myFNacro.value['ch']=='Napon'){
+            this.ms.addNapon(this.myFNacro.value['contenu'])
+            console.log(this.ms.ContenuNapon)
+         }
+ 
+         if(this.myFNacro.value['ch']=='Nacro'){
+             this.ms.addNacro(this.myFNacro.value['contenu'])
+             console.log(this.ms.ContenuNacro)
+          }
+          if(this.myFNacro.value['ch']=='Kabore'){
+             this.ms.addKabore(this.myFNacro.value['contenu'])
+             console.log(this.ms.ContenuKabore)
+          }
+          if(this.myFNacro.value['ch']=='Some'){
+             this.ms.addSome(this.myFNacro.value['contenu'])
+             console.log(this.ms.ContenuSome)
+          }
+    }
+
+    onSubmitKabore(){
+        if(this.myFKabore.value['ch']=='Napon'){
+            this.ms.addNapon(this.myFKabore.value['contenu'])
+            console.log(this.ms.ContenuNapon)
+         }
+ 
+         if(this.myFKabore.value['ch']=='Nacro'){
+             this.ms.addNacro(this.myFKabore.value['contenu'])
+             console.log(this.ms.ContenuNacro)
+          }
+          if(this.myFKabore.value['ch']=='Kabore'){
+             this.ms.addKabore(this.myFKabore.value['contenu'])
+             console.log(this.ms.ContenuKabore)
+          }
+          if(this.myFKabore.value['ch']=='Some'){
+             this.ms.addSome(this.myFKabore.value['contenu'])
+             console.log(this.ms.ContenuSome)
+          }
+    }
+
+    onSubmitSome(){
+        if(this.myFSome.value['ch']=='Napon'){
+            this.ms.addNapon(this.myFSome.value['contenu'])
+            console.log(this.ms.ContenuNapon)
+         }
+ 
+         if(this.myFSome.value['ch']=='Nacro'){
+             this.ms.addNacro(this.myFSome.value['contenu'])
+             console.log(this.ms.ContenuNacro)
+          }
+          if(this.myFSome.value['ch']=='Kabore'){
+             this.ms.addKabore(this.myFSome.value['contenu'])
+             console.log(this.ms.ContenuKabore)
+          }
+          if(this.myFSome.value['ch']=='Some'){
+             this.ms.addSome(this.myFSome.value['contenu'])
+             console.log(this.ms.ContenuSome)
+          }
     }
 
 }
